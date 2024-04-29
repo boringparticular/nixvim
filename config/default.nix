05399a1e0dc9354801608d51b6b74348a7f7b504
Kai Mies <develop@regnavi.de>
2024-04-29 11:50:34 +0200

    Move keymaps into separate file

--- config/default.nix
+++ config/default.nix
@@ -1,97 +1,9 @@
 {pkgs, ...}: {
   imports = [
     ./colorscheme.nix
+    ./keymaps.nix
     ./options.nix
     ./plugins
-  ];
-
-  keymaps = [
-    {
-      mode = "n";
-      key = "<Esc>";
-      action = "<cmd>nohlsearch<CR>";
-    }
-    {
-      mode = "n";
-      key = "[d";
-      action = "vim.diagnostic.goto_prev";
-      lua = true;
-      options.desc = "Go to previous [D]iagnostic message";
-    }
-    {
-      mode = "n";
-      key = "]d";
-      action = "vim.diagnostic.goto_next";
-      lua = true;
-      options.desc = "Go to next [D]iagnostic message";
-    }
-    {
-      mode = "n";
-      key = "<leader>e";
-      action = "vim.diagnostic.open_float";
-      lua = true;
-      options.desc = "Show diagnostic [E]rror messages";
-    }
-    {
-      mode = "n";
-      key = "<leader>q";
-      action = "vim.diagnostic.setloclist";
-      lua = true;
-      options.desc = "Open diagnostic [Q]uickfix list";
-    }
-
-    {
-      mode = "t";
-      key = "<Esc><Esc>";
-      action = "<C-\\><C-n>";
-      options.desc = "Exit terminal mode";
-    }
-
-    {
-      mode = "n";
-      key = "<left>";
-      action = "<cmd>echo 'Use h to move !! '<CR>";
-    }
-    {
-      mode = "n";
-      key = "<right>";
-      action = "<cmd>echo 'Use l to move !! '<CR>";
-    }
-    {
-      mode = "n";
-      key = "<up>";
-      action = "<cmd>echo 'Use k to move !! '<CR>";
-    }
-    {
-      mode = "n";
-      key = "<down>";
-      action = "<cmd>echo 'Use j to move !! '<CR>";
-    }
-
-    {
-      mode = "n";
-      key = "<C-h>";
-      action = "<C-w><C-h>";
-      options.desc = "Move focus to the left window";
-    }
-    {
-      mode = "n";
-      key = "<C-l>";
-      action = "<C-w><C-l>";
-      options.desc = "Move focus to the right window";
-    }
-    {
-      mode = "n";
-      key = "<C-j>";
-      action = "<C-w><C-j>";
-      options.desc = "Move focus to the lower window";
-    }
-    {
-      mode = "n";
-      key = "<C-k>";
-      action = "<C-w><C-k>";
-      options.desc = "Move focus to the upper window";
-    }
   ];
 
   autoGroups = {
