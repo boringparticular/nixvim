d4945471b44c40e673372b7156efd20e5da1f7b5
Kai Mies <develop@regnavi.de>
2024-04-29 11:13:12 +0200

    Add neovim nightly

--- flake.nix
+++ flake.nix
@@ -5,11 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = {
     nixvim,
     flake-parts,
+    neovim-nightly-overlay,
     ...
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -37,6 +39,11 @@
         };
         nvim = nixvim'.makeNixvimWithModule nixvimModule;
       in {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [neovim-nightly-overlay.overlay];
+        };
+
         checks = {
           # Run `nix flake check .` to verify that your config is not broken
           default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
