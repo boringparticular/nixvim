8e54ab32520ffe3949004e129cc6fe93015b697a
Kai Mies <develop@regnavi.de>
2024-04-30 11:32:19 +0200

    Remove unused keymaps from cmp

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -12,10 +12,6 @@
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-y>" = "cmp.mapping.confirm({ select = true })";
 
-          # "<CR>" = "cmp.mapping.confirm({ select = true })";
-          # "<Tab>" = "cmp.mapping.select_next_item()";
-          # "<S-Tab>" = "cmp.mapping.select_prev_item()";
-
           "<C-Space>" = "cmp.mapping.complete()";
 
           "<C-l>" = ''
