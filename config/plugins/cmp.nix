af57d6b8c13ef3b46e2f7a4908fe9a92814de7ea
Kai Mies <develop@regnavi.de>
2024-04-30 11:57:48 +0200

    Add annotations for injections

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -24,20 +24,28 @@
 
           "<C-Space>" = "cmp.mapping.complete()";
 
-          "<C-l>" = ''
-            cmp.mapping(function()
-              if require('luasnip').expand_or_locally_jumpable() then
-                require('luasnip').expand_or_jump()
-              end
-            end, { 'i', 's' })
-          '';
-          "<C-h>" = ''
-            cmp.mapping(function()
-              if require('luasnip').locally_jumpable(-1) then
-                require('luasnip').jump(-1)
-              end
-            end, { 'i', 's' })
-          '';
+          "<C-l>" =
+            /*
+            lua
+            */
+            ''
+              cmp.mapping(function()
+                if require('luasnip').expand_or_locally_jumpable() then
+                  require('luasnip').expand_or_jump()
+                end
+              end, { 'i', 's' })
+            '';
+          "<C-h>" =
+            /*
+            lua
+            */
+            ''
+              cmp.mapping(function()
+                if require('luasnip').locally_jumpable(-1) then
+                  require('luasnip').jump(-1)
+                end
+              end, { 'i', 's' })
+            '';
         };
         sources = [
           {name = "cody";}
